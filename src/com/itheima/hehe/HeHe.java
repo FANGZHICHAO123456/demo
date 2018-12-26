dccd8d1fc9b3c4415b946d04828795316a08d058
fangzhichao <928295647@qq.com>
2018-12-26 22:56:33 +0800

    HHH

---
+++ src/com/itheima/hehe/HeHe.java
@@ -0,0 +1,11 @@
+package com.itheima.hehe;
+
+public class HeHe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("呵呵");
+		System.out.println("哈哈");
+	}
+
+}
